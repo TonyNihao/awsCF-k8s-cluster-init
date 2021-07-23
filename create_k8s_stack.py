bb10c7d51782837d45f767df273ec1c2bb13adda
TonyNihao <tony@mail.com>
2021-07-24 01:51:24 +0300

    fix errors with stack creation

--- create_k8s_stack.py
+++ create_k8s_stack.py
@@ -5,8 +5,7 @@
 ### Usage:
 ### Run from the user which have aws cli access to the cloudformation. Please Google for configure it.
 ### $ python3 create_k8s_stack.py
-### enter your stack name (can be anything what you want) and enter path to the CF script if you have another
-### or enter ./k8s-stack.yml
+### enter your stack name (can be anything what you want) 
 ###
 ###
 ### https://github.com/TonyNihao
@@ -18,17 +17,38 @@ import os
 import argparse
 import sys
 import subprocess
+from time import sleep
 
 ### config section ###
 ## public ips keys 
 public_ip_keys = ["PublicIpMasterA", "PublicIpWorkerA", "PublicIpWorkerB", "PublicIpWorkerC"]
+##
+## default CF template path
+template_path = './k8s_stack.yml'
 ### end of config section ###
 
 def create_stack(stack_name, template_path):
     os.system('aws cloudformation create-stack \
                 --stack-name {0} \
                 --template-body file://{1}'.format(stack_name, template_path))
-
+    sleep(3)
+    run = True
+    while run:
+        check_completion_command = "aws cloudformation describe-stacks \
+                            --stack-name {0} | \
+                            jq -r '.Stacks[] | .StackStatus'".format(stack_name)
+        check_completion = subprocess.check_output(check_completion_command, text=True, shell=True).replace('\n', '')
+        if  check_completion == 'CREATE_IN_PROGRESS':
+            print(check_completion)
+            print('stack creation in progress wait...')
+            sleep(5)
+        elif check_completion == 'CREATE_COMPLETE':
+            print(check_completion)
+            print('stack created successfuly')
+            run = False
+        else:
+            print(check_completion)
+            exit(1)
 
 def get_outputs_values(stack_name, values):
     
@@ -69,11 +89,13 @@ def run_playbooks():
 
 def main():
     stack_name = input('Enter the stack name: ')
-    template_path = input('Enter the template full path: ')
 
+    print('Start create stack')
+    create_stack(stack_name=stack_name, template_path=template_path)
     outputs = get_outputs_values(stack_name, values=public_ip_keys)
     create_ansible_host_file(outputs)
     run_playbooks()
+    print(outputs)
 
 
 if __name__ == '__main__':
