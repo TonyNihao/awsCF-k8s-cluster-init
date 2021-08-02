f1629563611e7fd050c81102b140cbf27f936d25
TonyNihao <tony@mail.com>
2021-08-02 23:17:37 +0300

    add network plugin installation, fix package installation

--- ansible/scripts/install_docker.sh
+++ ansible/scripts/install_docker.sh
@@ -2,6 +2,7 @@
 # script that runs 
 # https://kubernetes.io/docs/setup/production-environment/container-runtime
 
+sudo yum update -y && sudo yum upgrade -y
 sudo amazon-linux-extras install docker -y
 sudo usermod -a -G docker ec2-user
 
